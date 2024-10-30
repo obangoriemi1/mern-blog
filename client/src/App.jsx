1d76c43b25c6da5232fe3ba867f1817a331ce0d7
kuchobang <obangoriemi@gmail>
2024-10-30 21:47:47 +0300

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Signin } from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import Projeects from "./pages/Projeects"
 function App() {
 
 
   return (
-    <div>
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/sign-in" element={<Signin/>}></Route>
+        <Route path="/sign-up" element={<Signup/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/projects" element={<Projeects/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Projeects.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projeects = () => {
+  return (
+    <div>Projeects</div>
+  )
+}
+
+export default Projeects
