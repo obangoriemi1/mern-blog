56dcb823a4f2e713383dc90d4add2045f29709a0
kuchobang <obangoriemi@gmail>
2024-11-04 17:27:37 +0300

    complete update api route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
 import authroutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 dotenv.config()
 
 
 const app = express()
 app.use(express.json()) 
+app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO)
 .then(() =>{
--- client/src/componenents/DashProfile.jsx
+++ client/src/componenents/DashProfile.jsx
@@ -1,24 +1,41 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+
 
 export const DashProfile = () => {
-    const {currentUser} = useSelector(state => state.user)
+const {currentUser} = useSelector((state) => state.user)
+ const [imageFile, setImageFile] = useState(null)
+ const [imageUrl, setImageUrl] = useState(null)
+ const filePickerRef = useRef()
+
+ const hanldeImageChane = (e) =>{
+    const file = e.target.files[0]
+    if(file){
+        setImageFile(file)
+        setImageUrl(URL.createObjectURL(file))
+    }
+   
+ }
+   
+   
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center text-3xl font-semibold '>Profile</h1>
         <form  className='flex flex-col gap-4 '>
-            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-            <img className='rounded-full w-full border-8 border-lime-600 object-cover ' src={currentUser.profilePicture} alt='user'/>
+            <input type="file" accept='image/*' onChange={hanldeImageChane} ref={filePickerRef} hidden/>
+            <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() =>filePickerRef.current.click()} >
+            <img className='rounded-full w-full border-8 border-[lightgray] object-cover ' src={imageUrl || currentUser.profilePicture} alt='user'/>
             </div>
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
-            <TextInput type='password' id='password' placeholder='password'/>
+            <TextInput type='password' id='password' />
            <Button gradientDuoTone='tealToLime' outline type='submit'>Update</Button>
         </form>
         <div className='text-red-600 flex justify-between mt-5'>
             <span className='cursor-pointer'>Delete Account</span>
-            <span className='cursor-pointer'>Log out</span>
+            <span className='cursor-pointer'>Log Out</span>
         </div>
     </div>
   )
